99d37aa7e9b58e6236e6b3edd4cf18a9d93e0e39
Checho <checho9999@gmail.com>
2020-02-12 14:18:19 -0300

    Incorporando a la interfaz los componentes de Gastos y de Presupuesto Restante; Utilizando useEffect para relacionar y modificar el presupuesto restante en base al disponible; Utilizando helpers para visualizar el restante; Documentando con PropTypes;

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Pregunta from './components/Pregunta'
 import Formulario from './components/Formulario';
+import Listado from './components/Listado'
+import ControlPresupuesto from './components/ControlPresupuesto'
 
 function App() {
 
@@ -8,15 +10,39 @@ function App() {
   const [ restante, guardarRestante ] = useState(0)
   const [ mostrarpregunta, actualizarPregunta ] = useState(true)
   const [ gastos, guardarGastos ] = useState([])
+  const [ gasto, guardarGasto] = useState({})
+  const [ creargasto, guardarCrearGasto] = useState(false)
 
-  //cuando agreguemos un nuevo gasto
+  //useEffect para actualizar el presupueto restante (aca movi el useState de "agregarNuevoGasto")
+  useEffect(() => {
+    if (creargasto){
+
+      // agrega el nuevo presupuesto
+      guardarGastos([
+        ...gastos,
+        gasto
+      ]);
+
+      //resta del presupuesto actual
+      const presupuestoRestante = restante - gasto.cantidad;
+      guardarRestante(presupuestoRestante);
+
+      //reseateamos a false    
+      guardarCrearGasto(false);
+    } 
+
+  }, [gasto, creargasto, gastos, restante]);
+
+  /*
+  //cuando agreguemos un nuevo gasto (movi el interior a al useEffect del restante)
   const agregarNuevoGasto = gasto => {
     //console.log(gasto);
     guardarGastos([
       ...gastos,
-      gastos    
+      gasto //Sin querer habia puesto gastos aca...por eso no me lo agregaba al state, :P    
     ])
   }
+  */
 
   return (
     <div className="container">
@@ -36,12 +62,20 @@ function App() {
               <div className="row">
                 <div className="one-half column">
                   <Formulario 
-                    agregarNuevoGasto={agregarNuevoGasto}
+//                  agregarNuevoGasto={agregarNuevoGasto}
+                    guardarGasto={guardarGasto}
+                    guardarCrearGasto={guardarCrearGasto}
                   />
                 </div>
 
                 <div className="one-half column">
-                  Listado de Gastos
+                  <Listado
+                    gastos={gastos}
+                  />
+                  <ControlPresupuesto
+                    presupuesto={presupuesto}
+                    restante={restante}
+                  />
                 </div>
               </div>                 
             )
